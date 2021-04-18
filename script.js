acd374d9c488e8913a37e868df8e2a4fe190ba62
Joshua Malone <jcharlesmalone@gmail.com>
2021-04-17 17:54:47 -0700

    hmm

--- script.js
+++ script.js
@@ -1,162 +1,100 @@
-var i = 0;
-var score = 0;
-var secondsLeft = 60;
-var timer = document.querySelector("#time");
-var messageDiv = document.querySelector("#message");
-var storedScores;
-var scoreList = [];
-var answerOne = document.getElementById("answerOne");
-var answerTwo = document.getElementById("answerTwo");
-var answerThree = document.getElementById("answerThree");
+var test = document.getElementById("test");
+var question = document.getElementById("questions")
+var potentialA = document.getElementById("potentialCorrects")
+var points = document.getElementById("points")
 
+var answer1 = document.getElementById("answer1");
+var answer2 = document.getElementById("answer1");
+var answer3 = document.getElementById("answer3");
+var answer4 = document.getElementById("answer4");
 
+var timeLeft = document.getElementById("TimeLeft")
+var userName = document.getElementById("userName")
+var correct = document.getElementById("Correct")
+var container = document.getElementById("container")
 
+var questions = [
+  {
+    question: "What is javaScript:",
+    a:"is a scripting or programming language that allows you to implement complex features on web pages ",
+     b:"is the standard markup language for Web pages. ", 
+     c:"elements are represented by <> tags.",
+    Correct: "is a scripting or programming language that allows you to implement complex features on web pages "
+  },
+  {
+    question: "what is the id symbol?",
+    a:"6",
+     b:"#", 
+   c: "<class>", 
+   d:"<script>",
+    Correct: "#"
+  },
+  {
+    question: "Where should you insert JavaScript?",
+    a:"the <head>", 
+    b:"the bottom of the <body>", 
+    c:"anywhere in the HTML",
+    d: "anywhere in the stylesheet",
+    Correct: "the bottom of the <body>"
+  },
+  {
+    question: "what is a function in JavaScript?",
+    a:"how you start a body syntex", 
+    b:" is similar to a procedure—a set of statements that performs a task or calculates a value", 
+    c: "elements are represented by <> tags.", 
+    d:"a command in html",
+    Correct: " is similar to a procedure—a set of statements that performs a task or calculates a value,"
+  },
+  {
+    question: "what does html stand for?",
+    a:"Hyper Text Makeup Language", 
+    b:"Hyper Transaction Markup Language", 
+    c:"Hyper Text Markup List", 
+    d:"Hyper Text Markup Language",
+    Correct: "Hyper Text Markup Language"
+  },
+  {
+    question: "what is Jquery?",
+    a:"is a lightweight, write less, do more",
+    b: "JavaScript library",
+    c: "its a html shortcut ", 
+    d:"its a if statement",
+    Correct: "is a lightweight, write less, do more, JavaScript library"
+  }
 
+]
 
-function setTime() {
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
-        timer.textContent = "Timer " + secondsLeft;
+container.addEventListener("click", start)
 
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            alert("Out of Time");
-            questionEnder();
-        }
+function showQuestion(){
+  question.innerHTML="<h3>"+ questions[questionsfollowing].question+"</h3>";
+  answer1.textContent= questions[questionsfollowing].a
+  answer2.textContent= questions[questionsfollowing].b
+  answer3.textContent= questions[questionsfollowing].c
+  answer4.textContent= questions[questionsfollowing].d
 
-        else if (i === questions.length) {
-            clearInterval(timerInterval);
-        }
-    }, 1000)
-    return (score)
 }
-function questionEnder() {
+function showTimer(){
+  timeInv = setInterval(function(){
+    quizTimer--;
+    countdown.textContent = "Time: " +quizTimer;
+    if(quizTimer === 0){
+      clearInterval(timeInv);
+      endPage();
+    }
+    timeDone = 60 -quizTimer;
 
-    var scoreTag = document.createElement("h1");
-    var inputTag = document.createElement("input");
-    var submitButton = document.createElement("button");
-    score += secondsLeft * .1;
-    score = score.toFixed(2);
-    document.getElementById("question").textContent = "All Done!";
-    answerOne.remove();
-    answerTwo.remove();
-    answerThree.remove();
-    answerFour.remove();
-    document.body.children[1].appendChild(scoreTag);
-    document.getElementsByTagName("h1")[0].setAttribute("id", "score");
-    document.getElementById("score").textContent = "Your Score: " + score;
-    document.body.children[1].appendChild(inputTag);
-    document.getElementsByTagName("input")[0].setAttribute("id", "input-field");
-    submitButton.textContent = "Submit";
-    document.body.children[1].appendChild(submitButton);
-    submitButton.addEventListener("click", function (event) {
-        event.preventDefault();
-        var highScoreText = new Object();
-        highScoreText.name = inputTag.value.trim();
-        highScoreText.newScore = score;
-        storeScores(highScoreText);
-        window.location.href = "highScores.html";
-    });
+  }, 1000);
 }
-function questionSetter() {
-
-    answerOne.hidden = false;
-    answerTwo.hidden = false;
-    answerThree.hidden = false;
-    answerFour.hidden = false;
-
-    document.getElementById("startButton").hidden = true;
-    if (i === questions.length) {
-        questionEnder();
-    }
-    else {
-        document.getElementById("question").textContent = questions[i]["title"];
-        document.getElementById("answerOne").textContent = questions[i]["choices"][0];
-        document.getElementById("answerTwo").textContent = questions[i]["choices"][1];
-        document.getElementById("answerThree").textContent = questions[i]["choices"][2];
-        document.getElementById("answerFour").textContent = questions[i]["choices"][3];
-    }
+function start(){
+  container.style.display = "none";
+  showQuestion()
+  showTimer()
+  quiz.style.display = "block"
 }
 
-function storeScores(highScoreText) {
-    tempArray = JSON.parse(localStorage.getItem("scores"));
-    if (tempArray === null) {
-        scoreList.push(highScoreText);
-        localStorage.setItem("scores", JSON.stringify(scoreList));
-    }
-    else {
-        tempArray.push(highScoreText);
-        localStorage.setItem("scores", JSON.stringify(tempArray));
-    }
-}
+answer1.addEventListener("click", nextQuestion)
+answer2.addEventListener("click", nextQuestion)
+answer3.addEventListener("click", nextQuestion)
+answer4.addEventListener("click", nextQuestion)
 
-document.getElementById("startButton").addEventListener("click", questionSetter);
-document.getElementById("startButton").addEventListener("click", setTime);
-document.getElementById("startButton").addEventListener("click", function () {
-    messageDiv.textContent = "";
-});
-
-answerOne.hidden = true;
-answerTwo.hidden = true;
-answerThree.hidden = true;
-answerFour.hidden = true;
-
-document.getElementById("answerOne").addEventListener("click", function () {
-    if (questions[i]["choices"][0] === questions[i]["answer"]) {
-        messageDiv.textContent = "Correct!";
-        score++;
-       
-    }
-    else {
-        messageDiv.textContent = "Wrong!";
-        secondsLeft -= 10;
-      
-    }
-    i++;
-    questionSetter();
-})
-
-document.getElementById("answerTwo").addEventListener("click", function () {
-    if (questions[i]["choices"][1] === questions[i]["answer"]) {
-        messageDiv.textContent = "Correct!";
-        score++;
-       
-    }
-    else {
-        messageDiv.textContent = "Wrong!";
-        secondsLeft -= 10;
-      
-    }
-    i++;
-    questionSetter();
-})
-
-document.getElementById("answerThree").addEventListener("click", function () {
-    if (questions[i]["choices"][2] === questions[i]["answer"]) {
-        messageDiv.textContent = "Correct!";
-        score++;
-       
-    }
-    else {
-        messageDiv.textContent = "Wrong!";
-        secondsLeft -= 10;
-      
-    }
-    i++;
-    questionSetter();
-})
-
-document.getElementById("answerFour").addEventListener("click", function () {
-    if (questions[i]["choices"][3] === questions[i]["answer"]) {
-        messageDiv.textContent = "Correct!";
-        score++;
-       
-    }
-    else {
-        messageDiv.textContent = "Wrong!";
-        secondsLeft -= 10;
-      
-    }
-    i++;
-    questionSetter();
-});
